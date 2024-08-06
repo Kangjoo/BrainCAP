dcc9c5542f73ab7d635a8c1839280bea71d584e4
Sam Brege <samuel.brege@yale.edu>
2024-08-06 14:46:12 -0400

    feat: added func to concatenate bolds

---
+++ pyCAP/pycap_concatenate.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+from pycap_functions.pycap_loaddata_hcp import concatenate_data, concatenate_motion, parse_slist
+import nibabel as nib
+import logging
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path!")
+
+def file_path(path):
+    if os.path.exists(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"file {path} does not exist!")
+
+def local_path(path):
+    if path[0] != '/':
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"{path} must be a local path from the specified sessions_dir!")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-sl", "--sessions_list", required=True, type=file_path, help="Path to list of sessions")
+parser.add_argument("-sd", "--sessions_dir", required=True, type=dir_path, help="Data directory containing individual sessions")
+parser.add_argument("-b", "--bolds", type=local_path, required=True, help="Comma separated list of bolds to concatenate, must be inside each session directory")
+parser.add_argument("-mf", "--motionfiles", type=local_path, required=False, help="Comma separated list of motion files to concatenate if doing scrubbing, must be inside each session directory")
+parser.add_argument("-o", "--output", type=local_path, required=True, help="Where to place output concatenated bold files, must be inside each session directory")
+parser.add_argument("-om", "--outputmotion", type=local_path, required=False, help="Where to place output concatenated motion files, must be inside each session directory. Required for scrubbing")
+parser.add_argument("-ow", "--overwrite", type=str, required=False, default="no", help="Whether to overwrite existing files")
+parser.add_argument("-d", "--ndummy", type=int, default=0, help="Number of dummy frames to remove")
+parser.add_argument("-l", "--logpath", default='./prep_pycap.log', help='Path to output log', required=False)
+
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    filename=args.logpath,
+                    filemode='w')
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+slist = parse_slist(args.sessions_list)
+
+bold_list = args.bolds.strip(' ;').split(',')
+if args.motionfiles is not None:
+    logging.info("--motionfiles supplied, will run motion concatenation")
+    conc_motion = True
+    motion_list = args.motionfiles.strip(' ;').split(',')
+else:
+    logging.info("--motionfiles not supplied, skipping motion concatenation")
+    conc_motion = False
+
+logging.info(f"Beginning bold concatenation for sessions in {args.sessions_list}...")
+
+for session in slist:
+    logging.info(f"    Processing {session}")
+    bolds = [os.path.join(args.sessions_dir, session, bold) for bold in bold_list]
+    bolds_string = '\t\t\n'.join(bolds)
+    logging.info(f"        Searching for files:")
+    logging.info(bolds_string)
+    logging.info(f"        Running bold concatenation...")
+    conc_path = os.path.join(args.sessions_dir, session, args.output)
+    if os.path.exists(conc_path):
+        logging.info(f"           Warning: Existing concatenated bold file found")
+        if args.overwrite.lower() == 'yes':
+            logging.info(f"           overwrite=yes, overwriting...")
+            nib.save(concatenate_data(bolds, args.ndummy), conc_path)
+            logging.info(f"        File {conc_path} created!")
+        else:
+            logging.info(f"           overwrite=no, skipping...")
+    else:
+        nib.save(concatenate_data(bolds, args.ndummy), conc_path)
+        logging.info(f"        File {conc_path} created!")
+
+    if conc_motion:
+        logging.info(f"        Running motion concatenation...")
+        motions = [os.path.join(args.sessions_dir, session, motion) for motion in motion_list]
+        conc_path = os.path.join(args.sessions_dir, session, args.outputmotion)
+        if os.path.exists(conc_path):
+            logging.info(f"           Warning: Existing concatenated motion file found")
+            if args.overwrite.lower() == 'yes':
+                logging.info(f"           overwrite=yes, overwriting...")
+            else:
+                logging.info(f"           overwrite=no, skipping...")
+                continue
+        with open(conc_path, 'w') as f:
+            f.write('\n'.join(concatenate_motion(motions, args.ndummy)))
+        logging.info(f"        File {conc_path} created!")
+        
+
+
+    logging.info(f"        {session} concatenated sucessfully\n")
