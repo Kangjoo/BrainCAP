9573d8fdf34ba9eafe7dd013070e3e58043d8924
samuelbrege <sabrege@gmail.com>
2024-08-16 08:46:34 -0400

    feat: PyCap wrapper now loads params from config
    Due to the number of steps that can be run at once and the sheer
    number of parameters, it would be unwieldy to expect users to
    use command-line flags for all parameters.
    
    Now, params are specified in a config .yml file which may be
    slightly more complicated at first but will make the process
    much easier. see examples/config.py a for an example.

--- pyCAP/run_pycap.py
+++ pyCAP/run_pycap.py
@@ -1,341 +1,103 @@
+#!/usr/bin/env python
+
 import subprocess
 import os
-#from time import time, ctime
+import time
 import logging
 from datetime import datetime
 from pathlib import Path
 import argparse
-
-def remove_directory_tree(start_directory: Path):
-    """Recursively and permanently removes the specified directory, all of its
-    subdirectories, and every file contained in any of those folders."""
-    for path in start_directory.iterdir():
-        if path.is_file():
-            path.unlink()
-        else:
-            remove_directory_tree(path)
-    start_directory.rmdir()
-
-
-#source activate /gpfs/gibbs/pi/n3/software/env/pycap_env
-
-
-def dir_path(path):
-    if os.path.isdir(path):
-        return path
-    else:
-        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+import pprint
+import yaml
 
 def file_path(path):
+    """
+    Used for argparse, ensures file exists
+    """
     if os.path.exists(path):
         return path
     else:
         raise argparse.ArgumentTypeError(f"file {path} does not exist!")
+    
+#Main function
 
-def local_path(path):
-    if path[0] != '/':
-        return path
-    else:
-        raise argparse.ArgumentTypeError(f"{path} must be a local path from the specified sessions_dir!")
+timestamp = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.%f")
+pp = pprint.PrettyPrinter(indent=1)
+
+req_args = {"global":{}}
+schedulers = ['NONE','SLURM','PBS']
+
+defaults = {"global":{'scheduler':{'type':"NONE"}},
+            "concatenate_bolds":{'overwrite':'no'}}
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--config", type=file_path, required=True, help="Path to config file with PyCap parameters")
+parser.add_argument("--steps", type=str, help="Comma seperated list of PyCap steps to run. Can also be specified in config")
+args = vars(parser.parse_args())
 
-parser.add_argument("--sessionsfolder", type=dir_path, required=True, help="Path to directory containing session data.")
-parser.add_argument("--outputfolder", type=str, required=True , help="Output directory for analysis results.")
-parser.add_argument("--ptemplate", type=file_path, required=False , help="Path to parcellation template if using parcellated data.")
-parser.add_argument("--sessionslist", type=file_path, required=True, help="Path to text file containing line-seperated list of sessions.")
-parser.add_argument("--inputbold", type=str, required=True, help="Local path inside each session's folder to the input BOLD data. If there are multiple BOLDs per session, " \
-                    "then this will be the path of the saved concatenated BOLD file. Multiple BOLDs must be specified with the '--inputbolds' flag. ")
-parser.add_argument("--inputbolds", type=str, required=False, help="Comma seperated list of BOLD files to be concatenated if data has multiple BOLDs.")
-parser.add_argument("--inputmotion", type=str, required=False, help="Local path inside each session's folder to the input motion data if using scrubbing. If there are multiple BOLDs per session, " \
-                    "then this will be the path of the saved concatenated motion file. Multiple motion files must be specified with the '--inputmotions' flag. ")
-parser.add_argument("--inputmotions", type=str, required=False, help="Comma seperated list of BOLD files to be concatenated if data has multiple BOLDs.")
-parser.add_argument("--ndummy", type=int, default=0, help="Number of initial dummy frames to remove")
-parser.add_argument("--step", type=str, required=True, help="Comma seperated list of PyCap steps to run")
-parser.add_argument("--overwrite", type=str, default="no", help="Whether to overwrite existing data")
-parser.add_argument("--scheduler", type=str, default="none", help="Comma seperated Scheduler parameters, accepted schedulers are SLURM, PBS, and none.")
+with open(args['config'], 'r') as f:
+    config = yaml.safe_load(f)
 
-#pycap params
-parser.add_argument("--kvals", default="2-15", type=str, help="Either a range (eg. 2-5) or comma-seperated list (2,3,4,5) of k-values to use")
-parser.add_argument("--nsplits", default=1, type=int, help="Number of split-half validations to run.")
-parser.add_argument("--scrubbing", type=str, help="Use scrugging or not (y/n)")
-parser.add_argument( "--savecapimg", type=str, help="Save CAP images or not (y/n)")
-parser.add_argument("--kmethod", type=str, help="(sse/silhouette)")
-parser.add_argument( "--eventcombine", type=str, help="(average/interserction/union)")
-parser.add_argument( "--eventtype", type=str, help="activation/deactivation/both")
-parser.add_argument("--gsr", type=str, help="(y/n)")
-parser.add_argument("--ncluster", type=int, help="Number of clusters for k-means clustering")
-parser.add_argument( "--maxiter", type=int, help="Iterations for k-menas clustering")
-parser.add_argument("--motiontype", type=str, help="(dvarsm,dvarsme,fd)")
-parser.add_argument("--seedtype", type=str, help="(seedfree/seedbased)")
-parser.add_argument("--seedname", type=str, help="Seed name")
-parser.add_argument("--seedthreshtype", type=str, help="(T/P)")
-parser.add_argument("--seedthreshold", type=float, help="Signal threshold")
-parser.add_argument( "--subsplittype", type=str, help="random/days")
-parser.add_argument( "--randTthreshold", type=float, help="Random Time Signal threshold")
-parser.add_argument("--unit", type=str, help="(p/d)")  # parcel or dense
-parser.add_argument("--motionthreshold", type=float, help="Motion threshold")
-parser.add_argument("--motiondisplay", type=str,
-                    help="Display motio parameter or not (y/n)")
+#Combine command line args with config args, with command line overwriting
+args = config | args
 
-# parser.add_argument("--job-name", type=str, help="Scheduler parameter. This will be assigned as the job's name")
-# parser.add_argument("--mem-per-cpu", type=str, default="2G", help="Scheduler parameter. Amount of memory to assign to each CPU, more will be needed if using dense data.")
-#parser.add_argument()
-args = parser.parse_args()
+if 'steps' not in args.keys():
+    print("--steps parameter not supplied! Exiting...")
+    exit()
 
-sched_args = args.scheduler.split(',')
+if type(args['steps']) != list:
+    args['steps'] = args['steps'].split(',')
 
-disallowed = {'SLURM':['nodes','ntasks','array']}
+#Set global defaults
+args['global'] = defaults['global'] | args['global']
+parsed_args = {}
+parsed_args['global'] = args['global']
+#Check schedulers are the same and parse with defaults
+first=True
+for step in args['steps']:
+    if step not in defaults.keys():
+        print(f"--steps parameter: '{step}' invalid! Exiting...")
+        exit()
 
-sched_type = sched_args[0]
-
-#--scheduler=SLURM,time=0-2:00:00,mem-per-cpu=1000,partition=pi_anticevic,mail-type=all,nodes=1,cpus-per-task=1
-
-#input parameters
-sessionsfolder="/gpfs/gibbs/pi/n3/Studies/Connectome/subjects/"
-analysisfolder="/gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/devtest/prep_test/"
-parcfile="/gpfs/gibbs/pi/n3/Studies/Connectome/Parcellations/CABNP/CortexSubcortex_ColeAnticevic_NetPartition_wSubcorGSR_parcels_LR_ReorderedByNetworks_Zeroes.pscalar.nii"
-sessions="/gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/sublist/hcp_n25.csv"
-
-bolds="images/functional/bold2_Atlas_MSMAll_hp2000_clean_res-WB_demean.dtseries.nii," \
-    "images/functional/bold1_Atlas_MSMAll_hp2000_clean_res-WB_demean.dtseries.nii," \
-    "images/functional/bold4_Atlas_MSMAll_hp2000_clean_res-WB_demean.dtseries.nii," \
-    "images/functional/bold3_Atlas_MSMAll_hp2000_clean_res-WB_demean.dtseries.nii"
-
-motionfiles="images/functional/movement/bold2.bstats," \
-    "images/functional/movement/bold1.bstats," \
-    "images/functional/movement/bold4.bstats," \
-    "images/functional/movement/bold3.bstats"
-
-datafile="images/functional/bold2143_test.dtseries.nii"
-motionfile="images/functional/movement/bold2143_test.bstats"
-overwrite="yes"
-ndummy=100
-steps="pycap"
-
-timestamp = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.%f")
-
-# if overwrite=='yes' and os.path.exists(analysisfolder):
-#     remove_directory_tree(Path(analysisfolder))
-
-
-if not os.path.exists(analysisfolder):
-    os.makedirs(analysisfolder)
-
-maxiter = 1000
-
-#scheduler parameters
-job_name = '25test'
-mem = '8G'
-cpus = 4
-partition = 'pi_anticevic'
-time = '1:00:00'
-
-nsplits = 2
-k_range = [2, 5]
-par_k = 1 #Not currently used
-
-#calculated parameters
-array = [1, nsplits]
-k_vals = list(range(k_range[0],k_range[1]+1))
-k_n = len(k_vals)
-results_dir = os.path.join(analysisfolder, 'results/')
-group_dir = os.path.join(analysisfolder, 'groupdata/')
-outdir = os.path.join(results_dir, 'Ssplit_')
-datadir = os.path.join(group_dir, 'Ssplit_')
-
-step_list = steps.split(',')
-
-#PyCap setup
-if 'pre_pycap' in step_list:
-    slurm_out = f"/gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/time-analytics/slurm_output/prep_{job_name}-{timestamp}.out"
-    for path in [analysisfolder, results_dir, group_dir]:
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-    command = "#!/bin/bash\n"
-    command += f"#SBATCH -J setup_{job_name}\n"
-    command += f"#SBATCH --mem-per-cpu=40G\n" #If dense, needs more data than if parc
-    command += f"#SBATCH --cpus-per-task=1\n"
-    command += f"#SBATCH --partition={partition}\n"
-    command += f"#SBATCH --time=2:00:00\n"
-    command += f"#SBATCH --output={slurm_out}\n"
-    command += f"#SBATCH --nodes=1\n"
-    command += f"#SBATCH --ntasks=1\n"
-
-    #Attempt to concatenate bolds if multiple specified
-    if bolds is not None:
-        command += f"python /gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/time-analytics/pyCAP/pyCAP/pycap_concatenate.py "
-        command += f"--sessions_list {sessions} "
-        command += f"--sessions_dir {sessionsfolder} "
-        command += f"--bolds {bolds} "
-        command += f"--output {datafile} "
-        command += f"--ndummy {ndummy} "
-        command += f"--overwrite {overwrite} "
-        command += f"--logpath {analysisfolder}run_pycap.log "
-
-        if motionfiles is not None:
-            command += f"--motionfiles {motionfiles} "
-            command += f"--outputmotion {motionfile} "
-
-        command += f"\n"
-
-        command += f"wait\n"
-
-    for split in range(nsplits):
-        if not os.path.exists(f'{outdir}{split+1}'):
-            os.makedirs(f'{outdir}{split+1}')
-        if not os.path.exists(f'{datadir}{split+1}'):
-            os.makedirs(f'{datadir}{split+1}')
-
-        command += f"python /gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/time-analytics/pyCAP/pyCAP/pycap_Ssplit_run_hcp.py "
-        command += f"--homedir {sessionsfolder} "
-        command += f"--outdir {outdir}{split+1}/ "
-        command += f"--datadir {datadir}{split+1} "
-        command += f"--inputdata {datafile} "
-        command += f"--sublistfilen {sessions} "
-        command += f"--pscalarfilen {parcfile} "
-        command += f"--ncluster 0 "
-        command += f"--mink {k_range[0]} "
-        command += f"--maxk {k_range[1]} "
-        command += f"--maxiter {maxiter} "
-        command += f"--kmethod silhouette "
-        command += f"--savecapimg n "
-        command += f"--gsr y "
-        command += f"--unit p "
-        command += f"--seedtype seedfree "
-        command += f"--randTthreshold 100 "
-        command += f"--scrubbing y "
-        #command += f"--runorder 2134 "
-        command += f"--motionfile {motionfile} "
-        command += f"--motiontype fd "
-        command += f"--motionthreshold 0.5 "
-        command += f"--motiondisplay n "
-        command += f"--ndummy 100 "
-        command += f"--step prep "
-        command += f"--subsplittype random \n"
-
-    serr = subprocess.STDOUT
-    sout = subprocess.PIPE
-
-    run = subprocess.Popen(
-            "sbatch", shell=True, stdin=subprocess.PIPE, stdout=sout, stderr=serr, close_fds=True
-        )
-
-    run.stdin.write((command).encode("utf-8"))
-    out = run.communicate()[0].decode("utf-8")
-    run.stdin.close()
-
-    job_id_prep = out.split("Submitted batch job ")[1]
-    print("Running PyCAP with prep")
-    print("Follow prep command progress in:")
-    print(f"{slurm_out}")
-    run_prep = True
-else:
-    print("Running PyCAP without prep")
-    job_id_prep = None
-    run_prep = False
-
-print('\n')
-
-#PyCap
-if 'pycap' in step_list:
-    slurm_out = f"/gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/time-analytics/slurm_output/main_{job_name}-{timestamp}.out"
-
-    command = "#!/bin/bash\n"
-    command += f"#SBATCH -J {job_name}\n"
-    command += f"#SBATCH --mem-per-cpu={mem}\n"
-    command += f"#SBATCH --cpus-per-task={cpus}\n"
-    command += f"#SBATCH --partition={partition}\n"
-    command += f"#SBATCH --time={time}\n"
-    command += f"#SBATCH --output={slurm_out}\n"
-    command += f"#SBATCH --array={array[0]}-{array[1]}\n"
-    command += f"#SBATCH --nodes=1\n"
-    command += f"#SBATCH --ntasks={k_n}\n"
-
-
-    command += "PERM=${SLURM_ARRAY_TASK_ID} \n"
-
-    #Pycap step 1
-    for k in k_vals:
-        command += f"srun --ntasks=1 python /gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/time-analytics/pyCAP/pyCAP/pycap_Ssplit_run_hcp.py "
-        command += f"--homedir {sessionsfolder} "
-        command += f"--outdir {outdir}$PERM/ "
-        command += f"--datadir {datadir}$PERM/ "
-        command += f"--inputdata {datafile} "
-        command += f"--sublistfilen {sessions} "
-        command += f"--pscalarfilen {parcfile} "
-        command += f"--ncluster {k} "
-        command += f"--mink {k_range[0]} "
-        command += f"--maxk {k_range[1]} "
-        command += f"--maxiter {maxiter} "
-        command += f"--kmethod silhouette "
-        command += f"--savecapimg n "
-        command += f"--gsr y "
-        command += f"--unit p "
-        command += f"--seedtype seedfree "
-        command += f"--randTthreshold 100 "
-        command += f"--scrubbing y "
-        #command += f"--runorder 2134 "
-        command += f"--motionfile {motionfile} "
-        command += f"--motiontype fd "
-        command += f"--motionthreshold 0.5 "
-        command += f"--motiondisplay n "
-        command += f"--ndummy 100 "
-        command += f"--step step1 "
-        command += f"--subsplittype random &\n"
-
-    command += "wait\n"
-
-    #Pycap step 2
-    command += f"python /gpfs/gibbs/pi/n3/Studies/CAP_Time_Analytics/time-analytics/pyCAP/pyCAP/pycap_Ssplit_run_hcp.py "
-    command += f"--homedir {sessionsfolder} "
-    command += f"--outdir {outdir}$PERM/ "
-    command += f"--datadir {datadir}$PERM "
-    command += f"--inputdata {datafile} "
-    command += f"--sublistfilen {sessions} "
-    command += f"--pscalarfilen {parcfile} "
-    command += f"--ncluster {k} "
-    command += f"--mink {k_range[0]} "
-    command += f"--maxk {k_range[1]} "
-    command += f"--maxiter {maxiter} "
-    command += f"--kmethod silhouette "
-    command += f"--savecapimg n "
-    command += f"--gsr y "
-    command += f"--unit p "
-    command += f"--seedtype seedfree "
-    command += f"--randTthreshold 100 "
-    command += f"--scrubbing y "
-    # command += f"--runorder 2134 "
-    command += f"--motionfile {motionfile} "
-    command += f"--motiontype fd "
-    command += f"--motionthreshold 0.5 "
-    command += f"--motiondisplay n "
-    command += f"--ndummy 100 "
-    command += f"--step step2 "
-    command += f"--subsplittype random \n"
-
-    serr = subprocess.STDOUT
-    sout = subprocess.PIPE
-
-    
-    
-
-    if run_prep:
-        sbatch = f"sbatch --dependency afterok:{job_id_prep}"
-        print("Main PyCap job will wait on `prep` to finish")
-        print("Once complete, follow main command progress in:")
+    step_args = defaults[step] | args['global'] | args[step]
+    if 'type' in step_args['scheduler'].keys():
+        step_sched = step_args['scheduler']['type'].upper()
+        if step_sched not in schedulers:
+            print(f"ERROR: Invalid scheduler {step_sched} provided! Scheduler must be one of '{str(schedulers)}'. Exiting...")
+            exit()
     else:
-        print("Follow main command progress in:")
-        sbatch = f"sbatch"
+        print(f"ERROR: scheduler specified in config, but missing scheduler type!")
+        exit()
 
-    print(f"{slurm_out}")
+    if first: 
+        sched_type = step_sched
+    elif sched_type != step_sched:
+        print(f"ERROR: all specified schedulers must be of same type, expected {sched_type}, found {step_sched}!")
+        print(f"If scheduler type unspecified, assumed 'NONE'. Exiting...")
+        exit()
+
+    parsed_args[step] = step_args
+
+#Command orchestration
+
+#Contain list of commands to run, either as jobs or directly
+commands = []
+
+for step in args['steps']:
+
+    if step not in args.keys():
+        print(f"--steps parameter: '{step}' missing config parameters! Exiting...")
+        exit()
     
+    step_args = parsed_args[step]
+    
+    pp.pprint(step_args)
 
-    run = subprocess.Popen(
-            sbatch, shell=True, stdin=subprocess.PIPE, stdout=sout, stderr=serr, close_fds=True
-        )
 
-    run.stdin.write((command).encode("utf-8"))
-    run.stdin.close()
+    if step == "concatenate_bolds":
+        pass
+
+    else:
+        print(f"--steps parameter: '{step}' invalid! Exiting...")
+        exit()
