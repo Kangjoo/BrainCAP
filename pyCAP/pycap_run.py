aafc47faf8b362881b119153f717111398a60e1c
samuelbrege <sabrege@gmail.com>
2024-09-27 17:17:00 -0400

    feat: pycap run now functional

--- pyCAP/pycap_run.py
+++ pyCAP/pycap_run.py
@@ -53,20 +53,20 @@ def local_path(path):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--scrubbing", type=str, help="Use scrugging or not (y/n)")
-parser.add_argument("--save_image", type=str, help="Save CAP images or not (y/n)")
-parser.add_argument("--k_method", type=str, help="(sse/silhouette)")
+parser.add_argument("--save_image", type=str, default='no', help="Save CAP images or not (y/n)")
+parser.add_argument("--k_method", default='silhouette', type=str, help="(sse/silhouette)")
 #SEEDPARAM, NEED TO ADD CODE FROM BITBUCKET #parser.add_argument("-ev", "--event_combine", type=str, help="(average/interserction/union)")
 #SEEDPARAM, NEED TO ADD CODE FROM BITBUCKET #parser.add_argument("-et", "--eventtype", type=str, help="activation/deactivation/both")
 parser.add_argument("--gsr", type=str, default="y", help="(y/n)")
 parser.add_argument("--sessions_folder", type=dir_path, help="Home directory path")
 parser.add_argument("--bold_path", type=local_path, help="Path to datafile inside session directory")
 parser.add_argument("--analysis_folder", type=dir_path, help="Output directory path")
-parser.add_argument("--n_splits", type=int, default=1, help="Number of splits to run, default 1")
+parser.add_argument("--split", type=int, default=1, help="Which split to run, default 1")
 #In wrapper script, derived from k range
 parser.add_argument("--n_k", type=int, help="Number of clusters for k-means clustering")
 #Shouldn't be necessary anymore? #parser.add_argument("--min_k", type=int, help="Mininum k for k-means clustering")
 #Shouldn't be necessary anymore? #parser.add_argument("--max_k", type=int, help="Maximum k for k-means clustering")
-parser.add_argument("--max_iter", type=int, help="Iterations for k-means clustering")
+parser.add_argument("--max_iter", type=int, default=1000, help="Max iterations for k-means clustering")
 parser.add_argument("--motion_type", type=str, help="(dvarsm,dvarsme,fd)")
 parser.add_argument("--motion_path", type=str, help="Path to motion file inside session directory")
 parser.add_argument("--seed_type", type=str, default="seedfree", help="(seedfree/seedbased), default 'seedfree'")
@@ -78,7 +78,7 @@ parser.add_argument("--sessions_list", required=True,
 parser.add_argument("--subsplit_type", default='random', type=str, help="random/days, default 'random'")
 parser.add_argument("--time_threshold", type=float, default=100, help="Random Time Signal threshold") #seedfree
 #NEED FIX FOR DENSE 
-parser.add_argument("--parc_template", type=file_path, required=False ,help="Path to parcellation template")
+parser.add_argument("--parc_file", type=file_path, required=False ,help="Path to parcellation template")
 parser.add_argument("--motion_threshold", type=float, help="Motion threshold")
 parser.add_argument("--display_motion", type=str,
                     help="Display motion parameter or not (y/n)")
@@ -96,7 +96,7 @@ formatter = logging.Formatter('%(asctime)s - %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
-logging.info("PyCap Prep Start")
+logging.info("PyCap Run Start")
 time.sleep(0.1)
 
 # -------------------------------------------
@@ -169,7 +169,7 @@ else:
 # # - parameters for k-means clustering
 param.kmean_k = args.n_k
 # param.kmean_krange = [args.min_k, args.max_k]
-# param.kmean_max_iter = args.max_iter
+param.kmean_max_iter = args.max_iter
 param.kmean_kmethod = args.k_method
 param.savecpimg = args.save_image
 
@@ -185,105 +185,91 @@ class FileIn:
 
 filein = FileIn()
 filein.sessions_folder = args.sessions_folder
-filein.sublist = parse_slist(args.seasions_list)
-filein.pscalar_filen = args.parc_template
+filein.sublist = parse_slist(args.sessions_list)
+filein.pscalar_filen = args.parc_file
 
 filein.fname = args.bold_path
 filein.motion_file = args.motion_path
 
-for split_i in range(args.n_splits):
-    #adjust to non-index count
-    split = split_i + 1
+#adjust to non-index count
+split = args.split
 
-    split_dir = os.path.join(args.analysis_folder, f"Ssplit{split}")
-        
-    filein.outpath = os.path.join(split_dir, f"{param.gsr}_{param.seedIDname}", 
-                                    f"{param.sig_thresholdtype}{str(param.time_threshold)}/")
-
-    filein.datadir = os.path.join(split_dir, f"{param.gsr}_{param.seedIDname}", 
-                                    f"{param.sig_thresholdtype}{str(param.time_threshold)}", "session_data/")
+split_dir = os.path.join(args.analysis_folder, f"Ssplit{split}")
     
-    for path in [filein.outpath, filein.datadir]:
+filein.outpath = os.path.join(split_dir, f"{param.gsr}_{param.seedIDname}", 
+                                f"{param.sig_thresholdtype}{str(param.time_threshold)}/")
 
-        try: 
-            os.makedirs(path)
-        except:
-            logging.info(f"Output folder {path} already exists")
-            if overwrite:
-                logging.info(f"    Overwrite '{args.overwrite}', overwriting...")
-                shutil.rmtree(path)
-                os.makedirs(path)
-            else:
-                logging.info(f"    Overwrite '{args.overwrite}', script will halt!")
-                logging.info(f"    To re-run prep, use overwrite 'yes' or delete {path}")
-                logging.info(f"--- STEP COMPLETE ---")
+filein.datadir = os.path.join(split_dir, f"{param.gsr}_{param.seedIDname}", 
+                                f"{param.sig_thresholdtype}{str(param.time_threshold)}", "session_data/")
 
+param.overwrite = args.overwrite
+# -------------------------------------------
+# - Population split-half list of subjects
+# -------------------------------------------
+
+test_sublist, training_sublist = subsplit(filein=filein, param=param)
+
+# -------------------------------------------
+# - Run the whole process for training and test datasets
+# -------------------------------------------
+for sp in [1, 2]:
+    if sp == 1:
+        param.spdatatag = "training_data"
+        filein.sublist = training_sublist
+    elif sp == 2:
+        param.spdatatag = "test_data"
+        filein.sublist = test_sublist
+
+    msg = "============================================"
+    logging.info(msg)
+    msg = "Start processing " + param.spdatatag + "..."
+    logging.info(msg)
+    msg = "    >> np.unique(filein.sublist) : " + str(np.unique(filein.sublist))
+    logging.info(msg)
+
+    # Setup output directory
+    # filein.analysis_folder = filein.outpath + param.spdatatag + "/"
+    # isExist = os.path.exists(filein.analysis_folder)
+    # if not isExist:
+    #     os.makedirs(filein.analysis_folder)
 
     # -------------------------------------------
-    # - Population split-half list of subjects
+    # - Load a time by space data matrix from individual and temporally concatenate
     # -------------------------------------------
 
-    test_sublist, training_sublist = subsplit(filein=filein, param=param)
+    data_all, sublabel_all = load_hpc_groupdata_wb_usesaved(filein=filein, param=param)
+    msg = "    >> np.unique(sublabel_all) : " + str(np.unique(sublabel_all))
+    logging.info(msg)
 
     # -------------------------------------------
-    # - Run the whole process for training and test datasets
+    # - Frame-selection to find the moments of activation
     # -------------------------------------------
-    for sp in [1, 2]:
-        if sp == 1:
-            param.spdatatag = "training_data"
-            filein.sublist = training_sublist
-        elif sp == 2:
-            param.spdatatag = "test_data"
-            filein.sublist = test_sublist
 
-        msg = "============================================"
-        logging.info(msg)
-        msg = "Start processing " + param.spdatatag + "..."
-        logging.info(msg)
-        msg = "    >> np.unique(filein.sublist) : " + str(np.unique(filein.sublist))
-        logging.info(msg)
+    # if param.seed_type == "seedbased":
+    #     # Reference: Liu and Duyn (2013), PNAS
+    #     seeddata_all = load_hpc_groupdata_seed_usesaved(filein=filein, param=param)
+    #     data_all_fsel, sublabel_all_fsel = frameselection_seed(
+    #         inputdata=data_all, labeldata=sublabel_all, seeddata=seeddata_all, filein=filein, param=param)
+    if param.seed_type == "seedfree":
+        # Reference: Liu et al. (2013), Front. Syst. Neurosci.
+        data_all_fsel, sublabel_all_fsel = frameselection_wb(
+            inputdata=data_all, labeldata=sublabel_all, filein=filein, param=param)
 
-        # Setup output directory
-        # filein.analysis_folder = filein.outpath + param.spdatatag + "/"
-        # isExist = os.path.exists(filein.analysis_folder)
-        # if not isExist:
-        #     os.makedirs(filein.analysis_folder)
+    msg = "    >> np.unique(sublabel_all_fsel) : " + str(np.unique(sublabel_all_fsel))
+    logging.info(msg)
 
-        # -------------------------------------------
-        # - Load a time by space data matrix from individual and temporally concatenate
-        # -------------------------------------------
+    clusterdata(inputdata=data_all_fsel, filein=filein, param=param)
 
-        data_all, sublabel_all = load_hpc_groupdata_wb_usesaved(filein=filein, param=param)
-        msg = "    >> np.unique(sublabel_all) : " + str(np.unique(sublabel_all))
-        logging.info(msg)
+    # -------------------------------------------
+    # - Delete variable to save space
+    # -------------------------------------------
+    del data_all, sublabel_all, data_all_fsel, sublabel_all_fsel
+    # if param.seed_type == "seedbased":
+    #     del seeddata_all
 
-        # -------------------------------------------
-        # - Frame-selection to find the moments of activation
-        # -------------------------------------------
-
-        # if param.seed_type == "seedbased":
-        #     # Reference: Liu and Duyn (2013), PNAS
-        #     seeddata_all = load_hpc_groupdata_seed_usesaved(filein=filein, param=param)
-        #     data_all_fsel, sublabel_all_fsel = frameselection_seed(
-        #         inputdata=data_all, labeldata=sublabel_all, seeddata=seeddata_all, filein=filein, param=param)
-        if param.seed_type == "seedfree":
-            # Reference: Liu et al. (2013), Front. Syst. Neurosci.
-            data_all_fsel, sublabel_all_fsel = frameselection_wb(
-                inputdata=data_all, labeldata=sublabel_all, filein=filein, param=param)
-
-        msg = "    >> np.unique(sublabel_all_fsel) : " + str(np.unique(sublabel_all_fsel))
-        logging.info(msg)
-
-        # -------------------------------------------
-        # - Delete variable to save space
-        # -------------------------------------------
-        del data_all, sublabel_all, data_all_fsel, sublabel_all_fsel
-        # if param.seed_type == "seedbased":
-        #     del seeddata_all
-
-        msg = "\n"
-        logging.info(msg)
+    msg = "\n"
+    logging.info(msg)
 
 
-    # - Notify job completion
-    logging.info(f"--- STEP COMPLETE ---")
+# - Notify job completion
+logging.info(f"--- STEP COMPLETE ---")
