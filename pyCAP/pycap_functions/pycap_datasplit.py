79e97bd390ad5191fdd68e72332ec65e25f4f5e9
samuelbrege <sabrege@gmail.com>
2024-11-20 12:03:58 -0500

    chore: renamed training/testing to split1/split2

--- pyCAP/pycap_functions/pycap_datasplit.py
+++ pyCAP/pycap_functions/pycap_datasplit.py
@@ -25,72 +25,72 @@ def subsplit(filein, param):
 
     filein.sublistfull = filein.sublistfull
     #splitdata_outfilen = filein.outdir + "subsplit_datalist.hdf5"
-    splitdata_outfilen = os.path.join(filein.datadir, "subsplit_datalist.hdf5")
+    splitdata_outfilen = os.path.join(filein.datadir, "sessions.hdf5")
     msg = splitdata_outfilen
     logging.info(msg)
     
     if os.path.exists(splitdata_outfilen):
 
-        msg = "File exists. Load existing list of subjects for training/test datasets: " + splitdata_outfilen
+        msg = "File exists. Load existing list of subjects for split_1/split_2 datasets: " + splitdata_outfilen
         logging.info(msg)
 
         f = h5py.File(splitdata_outfilen, 'r')
-        training_sublist_idx = f['training_sublist_idx']
-        test_sublist_idx = f['test_sublist_idx']
+        split_1_sublist_idx = f['split_1_sublist_idx']
+        split_2_sublist_idx = f['split_2_sublist_idx']
 
-        test_sublist = []
-        for index in test_sublist_idx:
-            test_sublist.append(filein.sublistfull[index])
-        training_sublist = []
-        for index in training_sublist_idx:
-            training_sublist.append(filein.sublistfull[index])
+        split_2_sublist = []
+        for index in split_2_sublist_idx:
+            split_2_sublist.append(filein.sublistfull[index])
+        split_1_sublist = []
+        for index in split_1_sublist_idx:
+            split_1_sublist.append(filein.sublistfull[index])
 
-        msg = "(Split 1) Training data subjects " + \
-            str(len(training_sublist)) + " : " + str(training_sublist)
+        msg = "(Split 1) split_1 data subjects " + \
+            str(len(split_1_sublist)) + " : " + str(split_1_sublist)
         logging.info(msg)
-        msg = "(Split 2) Test data subjects " + \
-            str(len(test_sublist)) + " : " + str(test_sublist)
+        msg = "(Split 2) split_2 data subjects " + \
+            str(len(split_2_sublist)) + " : " + str(split_2_sublist)
         logging.info(msg)
 
     else:
-        # generate list of subjects for training/test datasets
+        # generate list of subjects for split_1/split_2 datasets
         SubIdxlist = np.arange(0, len(filein.sublistfull))
         spsize = math.floor(SubIdxlist.shape[0]/2)
-        if param.subsplit_type == "random":
-            ms_kwargs = {"train_size": spsize, "test_size": spsize}
-            test_sublist_idx, training_sublist_idx = sklearn.model_selection.train_test_split(
-                SubIdxlist, **ms_kwargs)
+        #if param.subsplit_type == "random":
+        ms_kwargs = {"train_size": spsize, "test_size": spsize}
+        split_2_sublist_idx, split_1_sublist_idx = sklearn.model_selection.train_test_split(
+            SubIdxlist, **ms_kwargs)
 
-            test_sublist_idx = test_sublist_idx.tolist()
-            test_sublist = []
-            for index in test_sublist_idx:
-                test_sublist.append(filein.sublistfull[index])
-            training_sublist_idx = training_sublist_idx.tolist()
-            training_sublist = []
-            for index in training_sublist_idx:
-                training_sublist.append(filein.sublistfull[index])
+        split_2_sublist_idx = split_2_sublist_idx.tolist()
+        split_2_sublist = []
+        for index in split_2_sublist_idx:
+            split_2_sublist.append(filein.sublistfull[index])
+        split_1_sublist_idx = split_1_sublist_idx.tolist()
+        split_1_sublist = []
+        for index in split_1_sublist_idx:
+            split_1_sublist.append(filein.sublistfull[index])
 
 #         elif param.subsplit_type == "manual":
-#             training_sublist_idx = np.array(range(0, spsize, 1))
-#             test_sublist_idx = np.array(range(spsize, spsize*2, 1)) #example method for manual type
+#             split_1_sublist_idx = np.array(range(0, spsize, 1))
+#             split_2_sublist_idx = np.array(range(spsize, spsize*2, 1)) #example method for manual type
 
-        msg = "(Split 1) Training data subjects " + \
-            str(len(training_sublist)) + " : " + str(training_sublist)
+        msg = "(Split 1) split_1 data subjects " + \
+            str(len(split_1_sublist)) + " : " + str(split_1_sublist)
         logging.info(msg)
-        msg = "(Split 2) Test data subjects " + \
-            str(len(test_sublist)) + " : " + str(test_sublist)
+        msg = "(Split 2) split_2 data subjects " + \
+            str(len(split_2_sublist)) + " : " + str(split_2_sublist)
         logging.info(msg)
 
         # save output files
         f = h5py.File(splitdata_outfilen, "w")
-        dset1 = f.create_dataset("training_sublist_idx", (spsize,),
-                                 dtype=int, data=training_sublist_idx)
-        dset2 = f.create_dataset("test_sublist_idx", (spsize,), dtype=int, data=test_sublist_idx)
+        dset1 = f.create_dataset("split_1_sublist_idx", (spsize,),
+                                 dtype=int, data=split_1_sublist_idx)
+        dset2 = f.create_dataset("split_2_sublist_idx", (spsize,), dtype=int, data=split_2_sublist_idx)
         f.close()
         msg = "Saved Subject split data list indices in " + splitdata_outfilen
         logging.info(msg)
 
-    return test_sublist, training_sublist
+    return split_2_sublist, split_1_sublist
 
 
 
