a4219e2e95587ccb2f6d264b52390d744a89e205
samuelbrege <sabrege@gmail.com>
2024-12-02 13:08:37 -0500

    feat: get_seedtype function for checking seedtype

--- pyCAP/pycap_functions/pycap_utils.py
+++ pyCAP/pycap_functions/pycap_utils.py
@@ -1,6 +1,7 @@
 import logging
 import pycap_functions.pycap_exceptions as pe
 import numpy as np
+import os
 
 def handle_args(args, req_args, step=None, param=None):
     bad_arg = _check_args(args, req_args)
@@ -105,3 +106,34 @@ def index2id(to_convert, sublistfull):
     """
     sublist = np.asarray(sublistfull)
     return sublist[to_convert]
+
+def get_seedtype(seed):
+    """
+    Checks whether seed is a mask file or list of indices and checks incompatibilites
+    """
+    if seed.isinstance(list):
+        for val in seed:
+            seed_t = get_seedtype(val)
+            if seed_t != "index":
+                raise pe.StepError(step="loading seed",
+                                   error=f"seed list only compatible with type 'index', but found {seed_t}",
+                                   action="check parameters and documentation")
+        seed_t = "list"
+            
+    elif seed.isinstance(int):
+        seed_t = "index"
+
+    elif seed.isinstance(str):
+        if os.path.exists(seed):
+            seed_t = "file"
+        else:
+            raise pe.StepError(step="loading seed",
+                            error=f"attempted to load {seed} as a file but it does not exist",
+                           action="Check the supplied path exists and is not a local path.")
+    
+    else:
+        raise pe.StepError(step="loading seed",
+                           error=f"incompatible type for {seed}",
+                           action="seed must be a file, index, or list of indices")
+
+    return seed_t
