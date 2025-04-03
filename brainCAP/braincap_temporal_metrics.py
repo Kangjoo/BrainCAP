e9ba66331484fcba96c0125d3f94652ffe089fad
samuelbrege <sabrege@gmail.com>
2025-04-03 15:57:02 -0400

    feat: Temporal Metrics function

--- brainCAP/braincap_temporal_metrics.py
+++ brainCAP/braincap_temporal_metrics.py
@@ -53,20 +53,21 @@ def local_path(path):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--sessions_folder", type=dir_path, help="Home directory path")
 parser.add_argument("--analysis_folder", type=dir_path, help="Output directory path")
 parser.add_argument("--permutations", type=int, default=1, help="Range of permutations to run, default 1.")
-parser.add_argument("--sessions_list", required=True,
-                    help="Path to list of sessions", type=file_path)
+parser.add_argument("--sessions_list", required=True, help="Path to list of sessions", type=file_path)
 parser.add_argument("--overwrite", type=str, default="no", help='Whether to overwrite existing data')
 parser.add_argument("--log_path", default='./prep_run_hcp.log', help='Path to output log', required=False)
 parser.add_argument("--tag", default="", help="Tag for saving files, useful for doing multiple analyses in the same folder (Optional).")
+#parser.add_argument("--bold_type", default=None, help="BOLD data type (CIFTI/NIFTI), if not supplied will use file extention")
 
 
 args = parser.parse_args()  # Read arguments from command line
 
 if args.tag != "":
     args.tag += "_"
+overwrite = args.overwrite.lower()# == "yes"
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     filename=args.log_path,
@@ -77,7 +78,56 @@ console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
 logging.info("BrainCAP Temporal Metrics Start")
-#Wait for a moment so run_pycap.py log tracking can keep up
+#Wait for a moment so log tracking can keep up
 time.sleep(1)
 
+class Param:
+    pass
+
+param = Param()
+# if not args.bold_type:
+#     param.bold_type = utils.get_bold_type(args.bold_path)
+# else:
+#     param.bold_type = args.bold_type
+param.tag = args.tag
+param.overwrite = overwrite
+
+class FileIn:
+    pass
+
+
+filein = FileIn()
+filein.sublistfull, filein.groupsall = parse_sfile(args.sessions_list)
+
+basis_dir = os.path.join(args.analysis_folder, f"basis_CAPs")
+
+for sp in [1, 2]:
+    if sp == 1:
+        param.spdatatag = "split1"
+    elif sp == 2:
+        param.spdatatag = "split2"
+
+    basis_path = os.path.join(basis_dir, f"{args.tag}basis_CAP_split{sp}.hdf5")
+    f = h5py.File(basis_path, 'r')
+    basis_data = np.array(f['basis_CAP'])
+    logging.info("Basis CAP shape")
+    logging.info(basis_data.shape)
+
+    for perm_i in range(args.permutations):
+        perm = perm_i + 1
+        logging.info(f"Perm{perm}")
+        reorder_path = os.path.join(basis_dir, f"{args.tag}reordered_clustering_results_perm{perm}_split{sp}.hdf5")
+        r_f = h5py.File(reorder_path, 'r')
+        r_clmean = np.array(r_f["cluster_means"])
+        logging.info("Reorded cluster means shape")
+        logging.info(r_clmean.shape)
+        r_labels = np.array(r_f["cluster_labels"])
+        logging.info("Reorded cluster labels shape")
+        logging.info(r_labels.shape)
+        r_sublabels = utils.index2id(np.array(r_f["sublabel_all"]), filein.sublistfull)
+        logging.info("Reorded subject labels shape")
+        logging.info(r_sublabels.shape)
+        r_grouplabels = utils.index2id(np.array(r_f["grouplabel"]), filein.groupsall)
+        logging.info("Reorded group labels shape")
+        logging.info(r_grouplabels.shape)
 
